e72e0d85d37b43ce72ccb313fe21d0380a103968
ErwinSemorio <167678461+ErwinSemorio@users.noreply.github.com>
2025-10-23 00:51:24 +0800

    Delete Midtem directory

--- Midtem/MidtermQ9/Helpers/math_utils.py
+++
@@ -1,5 +0,0 @@
-def area(length: float, width: float) -> float:
-    """Return the area of a rectangle given length and width."""
-    if not (isinstance(length, (int, float)) and isinstance(width, (int, float))):
-        raise TypeError("Length and width must be numeric")
-    return length * width
